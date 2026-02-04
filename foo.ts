9f7c9af437f81aedc74d52abb1a1342a205e117e
Nathan Whitaker <nathan@deno.com>
2026-02-04 10:27:31 -0800

    remove some garbage files

--- foo.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2018-2025 the Deno authors. MIT license.
-import { OutgoingMessage, ServerResponse } from "node:http";
-
-console.log(ServerResponse, OutgoingMessage);
--- thing.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2018-2025 the Deno authors. MIT license.
-process.on("exit", () => {
-  console.log("exit");
-});
-console.log(import.meta.url);
